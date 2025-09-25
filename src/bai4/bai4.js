77c281f4ef850802ed49e7559f06b77c9591fc9f
HaNguyen051 <vanha03092005@gmail.com>
2025-09-25 15:14:32 +0700

    template string

---
+++ src/bai4/bai4.js
@@ -0,0 +1,10 @@
+const name = "ha" ; 
+const age = 20 ; 
+
+const introduction = "My name is " + name +  " \n and I'm " + age ; 
+console.log(introduction) ; 
+//template string 
+
+const intro = `My name is ${name}"test"
+I'm ${age}` ; 
+console.log(intro) ; 

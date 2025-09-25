d02d916731cee93bf1d8e954d5dc45d10c882d9e
HaNguyen051 <vanha03092005@gmail.com>
2025-09-25 21:51:47 +0700

    how to arrow function

---
+++ src/bai6/ham.js
@@ -0,0 +1,35 @@
+console.log("video 30") ; 
+
+function greeting(){
+    console.log("say hi") ; 
+}
+
+//call
+greeting() ; 
+
+ 
+ function sum1(a , b){ //params
+    console.log (a + b) ; 
+    //return a + b ; 
+ }
+sum1(4,7) ;//argument(input)
+sum1("ha" , " nguyen") ; 
+
+//arrow function 
+const sum = (a,b) => {
+    return a + b ; 
+}
+
+console.log(sum(4,7)) ; 
+
+// (function(){
+//     console.log("say hi ") ; 
+// })() ; 
+let sum3 = (a , b , c) => {
+    if(typeof a !== 'number')
+    {
+        return ;
+    }
+    return a  + b  + c; 
+}
+console.log(sum3(1,2,3)) ; 

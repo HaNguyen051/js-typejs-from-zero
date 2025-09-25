a0110ade5b166a0923c27ac9dd7e74393f2b2eb5
HaNguyen051 <vanha03092005@gmail.com>
2025-09-26 06:52:56 +0700

    update array

---
+++ src/chapter5/bai8/bai8.js
@@ -0,0 +1,28 @@
+//array
+const names = ["hoidanit" ,"eric" , "bla bla"] ; 
+const others = ["hoidanit" ,25 , "bla bla"] ; 
+console.log(names) ; 
+console.log(others) ; 
+
+//arr[index] 
+names[2] = "update name" ; 
+
+console.log(names[2]) ; 
+
+//Thêm phần tử
+/**
+ * cuối mảng : push()
+ * đầu mảng : unshift()
+ */
+
+//Xoá phần tử 
+/**
+ * cuối mảng :pop()
+ * đầu mảng :shift()
+ */
+
+names.pop() ;  
+console.log(names) ; 
+names.push(1 , "oke>") ; 
+console.log(names) ; 
+

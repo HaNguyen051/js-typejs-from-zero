49cd8b5379c7fe99d3de0284430becf307bf5b7c
HaNguyen051 <vanha03092005@gmail.com>
2025-09-25 14:48:50 +0700

    update toan tu

--- src/bai2/bai2.js
+++ src/bai2/bai2.js
@@ -10,4 +10,22 @@ console.log(person)
 //array
 
 const scores =[10,5,2,1,9] ; 
-console.log(scores)
+console.log(scores) 
+
+
+// toan tu 
+
+const a = 10; 
+const b = 20 ; 
+
+console.log(a + b) ; 
+let c  = a ; 
+
+console.log("c = " + c--) ; 
+
+//toan tu so sanh 
+
+const a1 = 5 ; //number
+const a2 = "5" ; //string
+
+console.log(a1 == a2) ; 

5c79e7af4bbbf6da865b7a9dc3f9da9535bf2bfb
HaNguyen051 <vanha03092005@gmail.com>
2025-09-25 21:10:25 +0700

    update sưitch & if

---
+++ bai5/bai5.js
@@ -0,0 +1,14 @@
+console.log('video 26') ; 
+const score =  8; 
+switch (true) {
+    case (score >= 8 && score <= 10) :
+        console.log("giỏi") ;
+        break;
+
+    case 6:
+        console.log("khá") ;
+        break;
+        
+    default:
+        console.log("yếu") ;
+}

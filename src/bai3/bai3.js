1ed8593de4f3e4e63732f6b4dd7d693b4538e253
HaNguyen051 <vanha03092005@gmail.com>
2025-09-25 15:02:52 +0700

    update chuoi

---
+++ src/bai3/bai3.js
@@ -0,0 +1,16 @@
+console.log("bai3") ; 
+//khai bao ten gan name 
+const name = 'hanguyen0' ; 
+//tuong tu khai bao name
+const age = 20 ; 
+
+console.error("oops my mistake") ; 
+
+console.warn("test warning") ; 
+
+let myName = 'Ha' ; 
+let myAge = '20' ;
+//cach cong chuoi 
+
+console.log("Name:" , myName, ", Age:" , myAge) ; 
+console.log(myName , myAge)

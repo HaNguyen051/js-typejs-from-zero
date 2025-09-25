e839472846e349d4fa30bc1fe3a8c92c8eb9ff7b
HaNguyen051 <vanha03092005@gmail.com>
2025-09-25 14:39:12 +0700

    first project"

---
+++ src/bai1/main.js
@@ -0,0 +1,8 @@
+console.log("video 17") ; 
+
+let name = 'hanguyen' ;
+
+const name1 = "hane"
+
+console.log(name)
+console.log(name1) ;
---
+++ src/bai2/bai2.js
@@ -0,0 +1,13 @@
+console.log("video19") ; 
+//luu tru 1 bien so co nhieu thong tin
+const person = {
+    name :  "hanguyen" , 
+    age: 18 , 
+    address: "hanoi"
+}
+console.log(person) 
+
+//array
+
+const scores =[10,5,2,1,9] ; 
+console.log(scores)

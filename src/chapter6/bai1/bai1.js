0eebb45bc1485093283551dba15a46a8b47a7780
HaNguyen051 <vanha03092005@gmail.com>
2025-09-26 12:09:05 +0700

    dom-stater

---
+++ src/chapter6/bai1/bai1.js
@@ -0,0 +1,20 @@
+
+console.log("video 49") ; 
+
+const element = document.getElementById("username") ; 
+console.log(element) ; 
+
+// const element2 =document.querySelector(".ichi")
+// console.log(element2) ; 
+
+const element2 = document.querySelectorAll(".ichi") ; 
+console.log(element2) ; 
+
+const doButton = document.getElementById("do") ; 
+console.log(doButton) ; 
+//tao event
+function handleOnClick()
+{
+    console.log("click r em yeu") ; 
+}
+

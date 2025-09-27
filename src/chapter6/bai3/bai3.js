b7cf149e62507a6ded280483ed73a0ef5751acc8
HaNguyen051 <vanha03092005@gmail.com>
2025-09-27 08:13:31 +0700

    style equal js

--- src/chapter6/bai3/bai3.js
+++ src/chapter6/bai3/bai3.js
@@ -6,11 +6,20 @@ const myText = document.getElementById('myTitle') ;
 myBtnElement.addEventListener('click' , ()=>{
     console.log('da kich hoat') ; 
     myText.innerText = "Move to page orther" ; 
+    myText.style.color = "red" ; 
+    myText.style.backgroundColor = 'green' ; 
+    myText.classList.add("text", 'hane') ; 
 })
 
 const backBtn = document.getElementById('backbut') ; 
 backBtn.addEventListener('click' , ()=>{
     myText.innerText = "Chào các bạn !" ; 
+    myText.style.color = "black" ; 
+    myText.style.backgroundColor = 'unset' ; 
+    myText.classList.remove("text", 'hane')
 })
 //inner html : co the truyen ca doan code html vao gom các thẻ 
 //inner text : chỉ thay đổi đoạn text đc gắn id 
+
+//style.  = "" ; 
+//classList.add("" , "") ; 

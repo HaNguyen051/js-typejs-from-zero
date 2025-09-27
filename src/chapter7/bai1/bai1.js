cc601abf14bf67d629db3c38ce575dfef1305cf4
HaNguyen051 <vanha03092005@gmail.com>
2025-09-27 19:05:48 +0700

    async_promise

---
+++ src/chapter7/bai1/bai1.js
@@ -0,0 +1,30 @@
+
+console.log("video62") ;
+
+// console.log("1") ; 
+// //async 
+// setTimeout(()=>{
+//     console.log("2") ; 
+// } , 5000) ; 
+// console.log("3") ; 
+
+// promise
+
+const myPromise = () => {
+    return new Promise((resolve , reject) =>{
+        setTimeout(()=>{
+        console.log("2 with promise") ; 
+        //return
+        resolve("hanguyen") ; 
+        } , 5000) ; 
+    })
+}
+const test = myPromise() ; 
+console.log("test" ,test );
+
+console.log("1") ;
+myPromise().then(data => {
+    console.log("data: " ,data) ; 
+    console.log("3") ; 
+}) ;
+

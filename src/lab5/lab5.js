8542ff3d088be749cdbabd88fb4fccf166a811fc
HaNguyen051 <vanha03092005@gmail.com>
2025-09-30 15:40:51 +0700

    lab5

---
+++ src/lab5/lab5.js
@@ -0,0 +1,35 @@
+
+//
+const fetchData  = async() => {
+    const tmp  = await fetch ("http://localhost:8000/users") ; 
+    const data = await tmp.json() ; 
+    console.log(data) ; 
+
+    //insert data to html 
+    const tbody = document.querySelector("#users tbody") ; 
+
+    if(data && data.length)
+    {   
+        let html = '' ; 
+        data.forEach((user , index) => {
+            html += `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+            </tr>
+        `;
+        });
+        tbody.innerHTML = html ; 
+    }
+    
+    
+}
+fetchData() ; 
+
+
+
+
+
+
+
